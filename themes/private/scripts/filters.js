25c7df10202a9e0c38645cc617372588d2d31b34
ym <yatmingtsai@foxmail.com>
2020-12-02 01:30:29 +0800

    更新 暂时关闭懒加载以修复 gif 显示慢

--- themes/private/scripts/filters.js
+++ themes/private/scripts/filters.js
@@ -1,5 +1,5 @@
-hexo.extend.filter.register('after_post_render', function (data) {
-  // 图片懒加载
-  data.content = data.content.replace(/<img(.*)>/g, '<img$1 loading="lazy">');
-  return data;
-});
+// hexo.extend.filter.register('after_post_render', function (data) {
+//   // 图片懒加载
+//   data.content = data.content.replace(/<img(.*)>/g, '<img$1 loading="lazy">');
+//   return data;
+// });
